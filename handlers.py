89f7fa01dfb680fe27cf9aa25eb4ac5d90745b49
dagim739 <dagimmulat739@gmail.com>
2025-08-15 23:21:46 -0900

    changed views to handlers

---
+++ handlers.py
@@ -0,0 +1,16 @@
+# define your handlers here
+
+def home(request, **kwargs):
+    return 'home'
+
+
+def contact(request , **kwargs):
+    return 'contact'
+
+
+def about(request, **kwargs):
+    return 'about'
+
+
+def services(request, **kwargs):
+    return 'services'
